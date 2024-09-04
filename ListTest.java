bd79e7a4b2ee566828096f71f5c1850b0e7e77af
jaqueline-Y <silvajaqueline9043@gmail.com>
2024-09-03 22:34:16 -0300

    ListTest.java

--- ListTest.java
+++ ListTest.java
@@ -1,10 +1,10 @@
-import javax.swing.*;
+import javax.swing.*; // Importa todas as classes do pacote javax.swing, que são usadas para criar interfaces gráficas em Swing.
 
-public class ListTest 
+public class ListTest // Declara uma classe pública chamada ListTest.
 {
-   public static void main(String[] args)
+   public static void main(String[] args) // Método main, ponto de entrada do programa Java.
    { 
-      ListFrame listFrame = new ListFrame();
-      listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      ListFrame listFrame = new ListFrame(); // Cria uma nova instância da classe ListFrame, que representa a janela da aplicação.
+      listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define o comportamento da janela ao ser fechada. Neste caso, encerra o programa quando a janela é fechada.
    } 
 }
