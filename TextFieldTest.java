add045dd8b8138b27c9abe8ce12cb1cbf2a7d1b0
Jaqueline <silvajaqueline9043@gmail.com>
2024-09-05 18:34:43 -0300

    TextFieldTest.java

--- TextFieldTest.java
+++ TextFieldTest.java
@@ -1,12 +1,20 @@
 import javax.swing.JFrame;
 
+// Classe TextFieldTest que contém o método main para iniciar a aplicação
 public class TextFieldTest
 {
    public static void main(String[] args)
    { 
+      // Cria uma instância da classe TextFieldFrame
       TextFieldFrame textFieldFrame = new TextFieldFrame(); 
+      
+      // Define a operação a ser realizada quando a janela for fechada (encerrar a aplicação)
       textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      // Define o tamanho da janela (largura: 350, altura: 100)
       textFieldFrame.setSize(350, 100); 
+      
+      // Torna a janela visível
       textFieldFrame.setVisible(true); 
    } 
 }
