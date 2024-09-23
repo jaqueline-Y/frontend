4daa8dfaf03d59f3694b7560f2b633f48d7a92cd
Jaqueline <silvajaqueline9043@gmail.com>
2024-09-23 09:54:02 -0300

    MenuTest.java

--- MenuTest.java
+++ MenuTest.java
@@ -1,5 +1,3 @@
-// Fig. 19.6: MenuTest.java
-// Testing MenuFrame.
 import javax.swing.JFrame;
 
 public class MenuTest
@@ -13,19 +11,4 @@ public class MenuTest
    } 
 } // end class MenuTest
 
-
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
